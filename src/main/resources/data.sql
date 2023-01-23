936f661b3909e1c0a09bf5e3c6fe40ab468beb42
Alexander Vinnik <v1nnik.ae@yandex.ru>
2023-01-24 02:05:13 +0700

    refactor:update sql schema

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,3 +12,13 @@ VALUES (1,'G'),
       (3,'PG-13'),
       (4,'R'),
       (5,'NC-17');
+
+MERGE INTO event_types (id, name)
+VALUES (1, "LIKE"),
+       (2, "REVIEW"),
+       (3, "FRIEND")
+
+MERGE INTO operations (id, name)
+VALUES (1, "REMOVE"),
+       (2, "REVIEW"),
+       (3, "FRIEND")
