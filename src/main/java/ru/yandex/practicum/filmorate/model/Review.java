35cfe10ad1557098cca8a718a85bb84781e5b18e
YuriyZanin <y.v.zanin@yandex.ru>
2023-01-24 15:35:14 +0400

    test commit 2

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -2,9 +2,12 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class Review {
     private Long id;
+    @NotNull
     private final User user;
     private final Film film;
     private String content;
