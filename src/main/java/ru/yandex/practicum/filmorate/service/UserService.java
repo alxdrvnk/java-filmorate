ea5f2c998d5c616e635f93ccee5050424ad1d19f
YuriyZanin <y.v.zanin@yandex.ru>
2023-01-29 22:33:16 +0400

    feat: реализовать алгорит поиска рекоммендаций

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -2,23 +2,24 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.dao.FilmDao;
+import ru.yandex.practicum.filmorate.dao.FilmLikeDao;
 import ru.yandex.practicum.filmorate.dao.UserDao;
 import ru.yandex.practicum.filmorate.dao.impl.FriendListDb;
 import ru.yandex.practicum.filmorate.exception.FilmorateNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserDao storage;
-
     private final FriendListDb friendListDb;
+    private final FilmDao filmDao;
+    private final FilmLikeDao likeDao;
 
     public User create(User user) {
         return storage.create(user);
@@ -63,18 +64,62 @@ public class UserService {
     }
 
     public List<Film> getRecommendations(Long userId) {
-        getUserBy(userId);
-        Map<Long, List<Long>> likes = storage.getAllLikes();
-        Map<Long, Map<Long, Integer>> freq = new HashMap<>();
-        for (Map.Entry<Long, List<Long>> pair : likes.entrySet()) {
-            for (Long filmId : pair.getValue()) {
-                if (!freq.containsKey(filmId)) {
-                    freq.put(filmId, new HashMap<>());
+        // достаем всех пользователей у которых есть хотя бы одно пересечение по лайкам с текущим пользователем
+        Map<Long, List<Long>> likes = likeDao.getSameLikesByUser(userId);
+        List<Long> userFilms = likes.remove(userId);
+
+        // если у текущего пользователя нет лайков или нет пересечений с другими, то и рекоммендаций быть не должно
+        if (userFilms == null || likes.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        // заполняем мапу с количеством пересечений (count:[userId]) с сортировкой по убыванию
+        Map<Integer, List<Long>> intersects = new TreeMap<>(Comparator.reverseOrder());
+        for (Map.Entry<Long, List<Long>> entry : likes.entrySet()) {
+            int intersectsCount = countingIntersects(userFilms, entry.getValue());
+            intersects.putIfAbsent(intersectsCount, new ArrayList<>());
+            intersects.get(intersectsCount).add(entry.getKey());
+        }
+
+        // находим пользователя с максимальным количеством пересечений,
+        // если количество пересечений совпадает с количеством фильмов,
+        // то этот юзер нам не подходит т.к. нечего рекомендовать, берем следующего
+        List<Long> similarUserFilms = new ArrayList<>();
+        for (Map.Entry<Integer, List<Long>> entry : intersects.entrySet()) {
+            for (Long currentUserId : entry.getValue()) {
+                List<Long> currentUserFilms = likes.get(currentUserId);
+                if (entry.getKey() < currentUserFilms.size()){
+                    similarUserFilms = currentUserFilms;
+                    break;
                 }
-
-
+            }
+            if (!similarUserFilms.isEmpty()){
+                break;
             }
         }
-        return null;
+
+        // находим фильмы, которые не пролайкал текущий пользователь.
+        List<Long> difference = findDifference(userFilms, similarUserFilms);
+        return filmDao.getByIds(difference);
+    }
+
+    private int countingIntersects(List<Long> userFilms, List<Long> anotherUserFilms) {
+        int count = 0;
+        for (Long filmId : userFilms) {
+            if (anotherUserFilms.contains(filmId)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private List<Long> findDifference(List<Long> userFilms, List<Long> anotherFilms) {
+        List<Long> diff = new ArrayList<>();
+        for (Long filmId : anotherFilms) {
+            if (!userFilms.contains(filmId)) {
+                diff.add(filmId);
+            }
+        }
+        return diff;
     }
 }
