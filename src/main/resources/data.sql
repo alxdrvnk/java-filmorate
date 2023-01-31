a3cd5807a9bb763207558a5e1f19659e90716395
Viktor Kurbatov <viktor-kurbatov@yandex.ru>
2023-01-31 06:35:15 +0300

    style: data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,24 +1,24 @@
-MERGE INTO genre (id, name)
-    VALUES (1, 'Комедия'),
-           (2, 'Драма'),
-           (3, 'Мультфильм'),
-           (4, 'Триллер'),
-           (5, 'Документальный'),
-           (6, 'Боевик');
+MERGE INTO genre (id,name)
+VALUES (1,'Комедия'),
+       (2,'Драма'),
+       (3,'Мультфильм'),
+       (4,'Триллер'),
+       (5,'Документальный'),
+       (6,'Боевик');
 
-MERGE INTO mpa (id, name)
-    VALUES (1, 'G'),
-           (2, 'PG'),
-           (3, 'PG-13'),
-           (4, 'R'),
-           (5, 'NC-17');
+MERGE INTO mpa (id,name)
+VALUES (1,'G'),
+      (2,'PG'),
+      (3,'PG-13'),
+      (4,'R'),
+      (5,'NC-17');
 
 MERGE INTO event_types (id, name)
-    VALUES (1, 'LIKE'),
-           (2, 'REVIEW'),
-           (3, 'FRIEND');
+VALUES (1, 'LIKE'),
+       (2, 'REVIEW'),
+       (3, 'FRIEND');
 
 MERGE INTO operations (id, name)
-    VALUES (1, 'REMOVE'),
-           (2, 'REVIEW'),
-           (3, 'FRIEND');
+VALUES (1, 'REMOVE'),
+       (2, 'REVIEW'),
+       (3, 'FRIEND');
